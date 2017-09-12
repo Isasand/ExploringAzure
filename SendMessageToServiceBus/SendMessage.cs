761cdacb89061fefe889684272707ea73fbc5642
isa sand <isasandh@gmail.com>
2017-09-12 06:32:06 +0200

    mistake fixed

--- SendMessageToServiceBus/SendMessage.cs
+++ SendMessageToServiceBus/SendMessage.cs
@@ -12,17 +12,23 @@ namespace Quenes
     {
         static void Main(string[] args)
         {
-            var connectionString = ""; 
-            var queueName = "";
-            var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+            
 
-            client.OnMessage(message =>
-            {
-                Console.WriteLine(String.Format("Message body: {0}", message.GetBody<String>()));
-                Console.WriteLine(String.Format("Message id: {0}", message.MessageId));
-            });
+            var connectionString = "Endpoint=sb://isasspace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=+ZGnzBDo8pgFWBOyyHhaywMeopvZQpx7oFA+LclLbOw=";
+            var queueName = "isasko";
+            var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+            
+            var message = new BrokeredMessage("This is a test message!");
+
+            Console.WriteLine(String.Format("Message id: {0}", message.MessageId));
+
+            client.Send(message);
+
+            Console.WriteLine("Message successfully sent! Press ENTER to read it");
+          
             Console.WriteLine("Press ENTER to exit program");
             Console.ReadLine();
         }
     }
 }
+ 
